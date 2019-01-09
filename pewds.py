dfd38f1a598aa7afd5ab4c32c49eabbafa9633b5
H-Kearney <46377523+H-Kearney@users.noreply.github.com>
2019-01-09 11:06:12 +0000

    The Main File!

---
+++ pewds.py
@@ -0,0 +1,31 @@
+import os, sys, webbrowser, chalk
+
+pewds = https://wwww.youtube.ie/user/pewdiepie
+
+tseries = https://wwww.youtube.ie/user/tseries
+
+confirmation1 = input("Do you want to Subscribe to PewDiePie? [Y/N] ")
+
+confirmation2 = input("Do you want to Unsubscribe from T-Series? [Y/N]")
+
+thanks = "thanks for helping us win the battle!"
+
+print (confirmation1)
+
+if confirmation1 == "y":
+    webbrowser.open(pewds)
+    print(chalk.blue(thanks))
+
+if else:
+    print ("you disgusting fuck")
+
+print (confirmation2)
+
+if confirmation2 == "y":
+    webbrowser.open(tseries)
+    print(chalk.blue(thanks))
+
+
+if else:
+    print ("you disgusting fuck")
+     
